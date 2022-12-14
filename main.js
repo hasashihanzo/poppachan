792721b17481f93a12857c2ed6d695f866d5c07f
hasashihanzo <87554604+hasashihanzo@users.noreply.github.com>
2022-12-14 21:50:12 +0700

    Update main.js
    Signed-off-by: hasashihanzo <87554604+hasashihanzo@users.noreply.github.com>

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ $('#view-work').on('click', function () {
         scrollTop: images
     }, 900)
 });
+jQuery(document).ready(function($){ 	
+	if($(".btn-top").length > 0){
+		$(window).scroll(function () {
+			var e = $(window).scrollTop();
+			if (e > 300) {
+				$(".btn-top").show()
+			} else {
+				$(".btn-top").hide()
+			}
+		});
+		$(".btn-top").click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			})
+		})
+	}		
+});
